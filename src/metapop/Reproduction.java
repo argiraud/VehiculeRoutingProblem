2ca6e106e6661107aa0779605575836ab025cef7
argiraud <a.giraud43@outlook.fr>
2020-06-06 11:45:38 +0200

    correction Reproduction

--- src/metapop/Reproduction.java
+++ src/metapop/Reproduction.java
@@ -10,9 +10,10 @@ public class Reproduction {
 
     public static List<Solution> getSelectedSolutions(List<Solution> solutionList) {
         int fitnessTotal = solutionList.stream().mapToInt(solution -> (int) solution.getDistanceTotal()).sum();
-        Map<Solution, Double> ratios = new HashMap<>();
         Double[] lastRatio = new Double[2];
+        List<Map<Solution, Double>> ratioss = new ArrayList<>();
         solutionList.forEach(solution -> {
+            Map<Solution, Double> ratios = new HashMap<>();
             if (ratios.isEmpty()) {
                 ratios.put(solution, (1D - (solution.getDistanceTotal() / fitnessTotal)));
             } else {
@@ -20,19 +21,19 @@ public class Reproduction {
                         + ratios.get(solutionList.get(ratios.size() - 1)));
             }
             lastRatio[0] = ratios.get(solution);
+            ratioss.add(ratios);
         });
-        return getRatioChoosenSolution(ratios, lastRatio);
+        return getRatioChoosenSolution(ratioss, lastRatio);
     }
 
-    private static List<Solution> getRatioChoosenSolution(Map<Solution, Double> ratios, Double[] lastRatio) {
+    private static List<Solution> getRatioChoosenSolution(List<Map<Solution, Double>> ratios, Double[] lastRatio) {
         List<Solution> choosenSolutions = new ArrayList<>();
         for (int i = 0; i < ratios.size(); i++) {
             double rationAlea = r.nextDouble() * lastRatio[0];
-            for (Map.Entry<Solution, Double> ratio : ratios.entrySet()) {
-                if (rationAlea <= ratio.getValue()) {
-                    choosenSolutions.add(ratio.getKey());
+            for (Map<Solution, Double> ratio : ratios) {
+                if (rationAlea <= ratio.entrySet().stream().findFirst().get().getValue()) {
+                    choosenSolutions.add(ratio.entrySet().stream().findFirst().get().getKey());
                     break;
-
                 }
             }
         }
